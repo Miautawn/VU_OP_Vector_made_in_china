9e11a883dc03f5a36ec054ce323e855c56a6c6bb
miautawn <miautawn@gmail.com>
2021-05-17 14:43:04 +0300

    final commiyt

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include "mVector.hpp"
+#include <iostream>
+#include <vector>
+#include "timer.h"
+
+using namespace std;
+
+int main()
+{
+    Timer timer;
+    // Pradėti v1 užpildymo laiko matavimą
+    unsigned int sz =  100000000;
+
+    int mem_keitimas_1 = 0;
+
+    timer.reset();
+    std::vector<int> v1;
+    for (int i = 1; i <= sz; ++i) {
+        if(v1.size() == v1.capacity()) kiek += 1;
+        v1.push_back(i);
+    }
+    cout<<"std::vector laikas su " << sz << ": "<< timer.current_time()<<endl;
+
+
+    int mem_keitimas_2 = 0;
+
+    timer.reset();
+    mVector<int> v2;
+    for (int i = 1; i <= sz; ++i) {
+        if(v2.size() == v2.capacity()) kiek2 += 1;
+        v2.push_back(i);
+    }
+    
+    cout<<"mVector laikas su " << sz << ": "<< timer.current_time()<<endl;
+    cout<<"std::vector pakeitimai: "<<mem_keitimas_1<<endl;
+    cout<<"mVector pakeitimai: "<<mem_keitimas_2<<endl;
+}
